fc1449d77593f4c032d5d22ed8faed93bc3399cc
ed system on behalf of Daniel Geng 🦃 <ed>
2025-03-12 12:42:23 +0000

    Scaffold update by instructor

--- sound_seg.c
+++ sound_seg.c
@@ -11,52 +11,52 @@ struct sound_seg {
 };
 
 // Load a WAV file into buffer
-void load(const char* filename, int16_t* dest){
+void wav_load(const char* filename, int16_t* dest){
     return;
 }
 
 // Create/write a WAV file from buffer
-void save(const char* fname, int16_t* src, size_t len){
+void wav_save(const char* fname, int16_t* src, size_t len){
     return;
 }
 
 // Initialize a new sound_seg object
-struct sound_seg* init() {
+struct sound_seg* tr_init() {
     return NULL;
 }
 
 // Destroy a sound_seg object and free all allocated memory
-void destroy(struct sound_seg* obj) {
+void tr_destroy(struct sound_seg* obj) {
     return;
 }
 
 // Return the length of the segment
-size_t length(struct sound_seg* seg) {
+size_t tr_length(struct sound_seg* seg) {
     return (size_t)-1;
 }
 
 // Read len elements from position pos into dest
-void read(struct sound_seg* track, int16_t* dest, size_t pos, size_t len) {
+void tr_read(struct sound_seg* track, int16_t* dest, size_t pos, size_t len) {
     return;
 }
 
 // Write len elements from src into position pos
-void write(struct sound_seg* track, int16_t* src, size_t pos, size_t len) {
+void tr_write(struct sound_seg* track, int16_t* src, size_t pos, size_t len) {
     return;
 }
 
 // Delete a range of elements from the track
-bool delete_range(struct sound_seg* track, size_t pos, size_t len) {
+bool tr_delete_range(struct sound_seg* track, size_t pos, size_t len) {
     return true;
 }
 
 // Returns a string containing <start>,<end> ad pairs in target
-char* identify(struct sound_seg* target, struct sound_seg* ad){
+char* tr_identify(struct sound_seg* target, struct sound_seg* ad){
     return NULL;
 }
 
 // Insert a portion of src_track into dest_track at position destpos
-void insert(struct sound_seg* src_track,
+void tr_insert(struct sound_seg* src_track,
             struct sound_seg* dest_track,
             size_t destpos, size_t srcpos, size_t len) {
     return;
